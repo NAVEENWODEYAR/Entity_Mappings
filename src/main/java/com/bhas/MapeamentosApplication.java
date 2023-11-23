cab66b5af869984cac201f2da2cc80474f0e3da8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-23 22:48:35 +0530

    initial commit

---
+++ src/main/java/com/bhas/MapeamentosApplication.java
@@ -0,0 +1,15 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentosApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MapeamentosApplication.class, args);
+	}
+
+}
