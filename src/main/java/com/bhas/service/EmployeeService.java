0e4923d872ce35c44a94f5b5e0986e51f4b63992
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-24 11:35:24 +0530

    CRUD operations,

--- src/main/java/com/bhas/service/EmployeeService.java
+++ src/main/java/com/bhas/service/EmployeeService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService
 {
@@ -16,9 +18,20 @@ public class EmployeeService
 
     // CRUD
     //1. INSERT INTO TABLE-NAME VALUES();
-    public ResponseEntity<String> insertEmployee(Employee employee)
+    public Employee insertEmployee(Employee employee)
     {
-        employeeRepo.save(employee);
-        return ResponseEntity.ok("Details saved,");
+        return employeeRepo.save(employee);
+    }
+
+    //1. a List<Employees>
+    public List<Employee> insertEmployees(List<Employee> employees)
+    {
+        return employeeRepo.saveAll(employees);
+    }
+
+    //2. SELECT * FROM TABLE-NAME;
+    public List<Employee> getEmployees()
+    {
+        return employeeRepo.findAll();
     }
 }
