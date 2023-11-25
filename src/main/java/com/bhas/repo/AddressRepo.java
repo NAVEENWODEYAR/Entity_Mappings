06d4c33643c96be979982b4fbce33eff0517e1c9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-25 18:31:17 +0530

    removed unwanted file

--- src/main/java/com/bhas/repo/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bhas.repo;
-
-import com.bhas.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepo extends JpaRepository<Address,Integer>
-{
-
-}
